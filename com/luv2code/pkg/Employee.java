9344fba4001b5714c0d1713c58e437ac90303e71
Great Joycs <76424765+pavigreatjoycs@users.noreply.github.com>
2023-03-14 09:49:25 +0530

    Update Employee.java

--- com/luv2code/pkg/Employee.java
+++ com/luv2code/pkg/Employee.java
@@ -1,4 +1,13 @@
 public class Employee{
   private int id;
   private String name;
+  
+  public int getId(){
+    return this.id;
+  }
+  
+  public String getName(){
+    return this.name;
+  }
+  
 }
